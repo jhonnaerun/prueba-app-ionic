2427ac7fa350b22b0c9c55a632406eac4c6343c8
jhonnatan <jhonnaerun@gmail.com>
2021-03-17 08:11:22 -0500

    Generación apk con nuevos resources

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  pathServer: 'http://localhost:3000'
-  // pathServer: 'http://192.168.1.60:3000'
+  //pathServer: 'http://localhost:3000'
+  pathServer: 'http://192.168.1.60:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  pathServer: 'http://localhost:3000'
-  // pathServer: 'http://192.168.1.60:3000'
+  // pathServer: 'http://localhost:3000'
+  pathServer: 'http://192.168.1.60:3000'
 };
 
 /*
